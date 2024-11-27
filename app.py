6db928992aee42819fdee5c8d33f365a8a8b82a5
edierGuerra <edierguerra55@gmail.com>
2024-11-27 15:23:24 -0500

    Agrega la pagina de proyectos

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route("/proyectos")
+def ruta():
+    return render_template("proyectos.html")
 #Iniciar app
 def run_app():
     return app.run(debug=True)
